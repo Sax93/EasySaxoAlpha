11a47b8f5bb283a8c1924ca1c73cf676ebdec489
Sax93 <mipanasax@gmail.com>
2026-08-17 00:39:07 -0400

    Fixed 1.052 Version

--- run.bat
+++ run.bat
@@ -1,4 +1,12 @@
 @echo off
 cd /d "%~dp0"
+
+if not exist ".venv\Scripts\python.exe" (
+    echo [ERROR] No se encontro el ejecutable Python en .venv\Scripts\python.exe
+    echo Por favor, verifica el nombre de tu entorno virtual o crealo con: python -m venv .venv
+    pause
+    exit /b
+)
+
 ".venv\Scripts\python.exe" -m easysaxo.main
 pause
