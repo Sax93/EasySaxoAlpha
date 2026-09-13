d7336e00b5898173c899495a0308974e36e6a195
Sax (SXF) <mipanasax@gmail.com>
2026-09-13 00:36:45 -0400

    Version 1.11.00

--- run.bat
+++ run.bat
@@ -1,14 +1,20 @@
 @echo off
 cd /d "%~dp0"
 
-if not exist ".venv\Scripts\python.exe" (
+set "PYTHON_ENV=.venv\Scripts\python.exe"
+if not exist "%PYTHON_ENV%" set "PYTHON_ENV=.venv\bin\python.exe"
+
+if not exist "%PYTHON_ENV%" (
     echo Action: Creating .venv directory...
-    python -m venv .venv
+    py -m venv .venv 2>nul || python -m venv .venv
+
+    set "PYTHON_ENV=.venv\Scripts\python.exe"
+    if not exist "%PYTHON_ENV%" set "PYTHON_ENV=.venv\bin\python.exe"
     
     echo Action: Upgrading pip and installing project dependencies...
-    ".venv\Scripts\python.exe" -m pip install --upgrade pip
-    ".venv\Scripts\python.exe" -m pip install -e .
+    "%PYTHON_ENV%" -m pip install --upgrade pip
+    "%PYTHON_ENV%" -m pip install -e .
 )
 
-".venv\Scripts\python.exe" -m easysaxo.main
+"%PYTHON_ENV%" -m easysaxo.main
 pause
